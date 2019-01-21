9ba51ab3e722a39adc6f91995297bb36304b7404
José F. R. Fonseca <jfrfonseca@users.noreply.github.com>
2019-01-21 19:09:58 +0000

    Included GLOB-matching of directories for copyright update test

--- test_update_copyright.py
+++ test_update_copyright.py
@@ -22,6 +22,7 @@ __version__    = '0.20180927.3'
 # Standard Library
 import os
 import sys
+import glob
 
 # Project
 from python_project_report_tool import git_tools, update_meta_tags
@@ -35,27 +36,36 @@ from python_project_report_tool import git_tools, update_meta_tags
 if __name__ == '__main__':
     home = os.getcwd()
     if len(sys.argv) > 2:
-        directory = sys.argv[1]
+        directory_list = sys.argv[1]
+        if not os.path.exists(directory_list):
+            directory_list = glob.glob(directory_list)
+            directory_list = [d for d in directory_list if os.path.isdir(d)]
+        else:
+            directory_list = [directory_list]
         copyright_message = sys.argv[2]
     else:
-        directory = os.getcwd()
+        directory_list = [os.getcwd()]
         copyright_message = sys.argv[1]
-    print("Performing tests in ", directory)
-    print("Copyright message: {}".format(copyright_message))
 
-    # Get current branch
-    current_branch = git_tools.get_current_branch(directory)
-    print("\t- Got current GIT branch: {}".format(current_branch))
+    print('Directories to perform tests on: {}'.format(directory_list))
 
-    # Process history
-    history = git_tools.get_git_history(root_directory=directory, branch=current_branch)
-    print("\t- Got GIT history")
-    file_history = git_tools.get_file_history(history)
-    print("\t- Got GIT file history")
-    for filename, metadata in file_history.items():
-        if filename.endswith('.py'):
-            path = os.path.join(directory, metadata['file'])
-            if metadata['current'] and os.path.isfile(path):
-                updated = update_meta_tags.update_copyright_message(path, copyright_message)
-                if updated:
-                    print('Updated {} in {}'.format(filename, path))
+    for directory in directory_list:
+        print("Performing tests in ", directory)
+        print("Copyright message: {}".format(copyright_message))
+
+        # Get current branch
+        current_branch = git_tools.get_current_branch(directory)
+        print("\t- Got current GIT branch: {}".format(current_branch))
+
+        # Process history
+        history = git_tools.get_git_history(root_directory=directory, branch=current_branch)
+        print("\t- Got GIT history")
+        file_history = git_tools.get_file_history(history)
+        print("\t- Got GIT file history")
+        for filename, metadata in file_history.items():
+            if filename.endswith('.py'):
+                path = os.path.join(directory, metadata['file'])
+                if metadata['current'] and os.path.isfile(path):
+                    updated = update_meta_tags.update_copyright_message(path, copyright_message)
+                    if updated:
+                        print('Updated {} in {}'.format(filename, path))
