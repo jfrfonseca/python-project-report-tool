0ef818cf34d526449c3602d9aa04c896eaf6376e
José F. R. Fonseca <jfrfonseca@users.noreply.github.com>
2019-01-23 12:38:20 +0000

    Included GLOB interface

--- test_update_date__version.py
+++ test_update_date__version.py
@@ -22,6 +22,7 @@ __version__    = ''
 # Standard Library
 import os
 import sys
+import glob
 import datetime
 
 # Project
@@ -37,37 +38,46 @@ from python_project_report_tool.versioning_schema.constant__day__commit_count im
 if __name__ == '__main__':
     home = os.getcwd()
     if len(sys.argv) > 2:
-        directory = sys.argv[1]
+        directory_list = sys.argv[1]
+        if not os.path.exists(directory_list):
+            directory_list = glob.glob(directory_list)
+            directory_list = [d for d in directory_list if os.path.isdir(d)]
+        else:
+            directory_list = [directory_list]
         major_version = sys.argv[2]
     else:
-        directory = os.getcwd()
+        directory_list = [os.getcwd()]
         major_version = sys.argv[1]
-    print("Performing tests in ", directory)
+
     print("Major version: {}".format(major_version))
+    print('Directories to perform tests on: {}'.format(directory_list))
 
-    # Get current branch
-    current_branch = git_tools.get_current_branch(directory)
-    print("\t- Got current GIT branch: {}".format(current_branch))
+    for directory in directory_list:
+        print("Performing tests in ", directory)
 
-    # Process history
-    history = git_tools.get_git_history(root_directory=directory, branch=current_branch)
-    print("\t- Got GIT history")
-    file_history = git_tools.get_file_history(history)
-    print("\t- Got GIT file history")
-    for filename, metadata in file_history.items():
-        if filename.endswith('.py'):
-            path = os.path.join(directory, metadata['file'])
-            if metadata['current'] and os.path.isfile(path):
+        # Get current branch
+        current_branch = git_tools.get_current_branch(directory)
+        print("\t- Got current GIT branch: {}".format(current_branch))
 
-                date = git_tools.parse_git_date(metadata['last_update'])
-                if date.date() != datetime.date.today():
+        # Process history
+        history = git_tools.get_git_history(root_directory=directory, branch=current_branch)
+        print("\t- Got GIT history")
+        file_history = git_tools.get_file_history(history)
+        print("\t- Got GIT file history")
+        for filename, metadata in file_history.items():
+            if filename.endswith('.py'):
+                path = os.path.join(directory, metadata['file'])
+                if metadata['current'] and os.path.isfile(path):
 
-                    date = date.strftime("%d/%m/%Y %H:%M:%S")
-                    updated = update_meta_tags.update_date(path, date)
-                    if updated:
-                        print('Updated date {} in {}'.format(filename, path))
+                    date = git_tools.parse_git_date(metadata['last_update'])
+                    if date.date() != datetime.date.today():
 
-                    version = __format_record__constant__day__commit_count(metadata, major_version)
-                    updated = update_meta_tags.update_version(path, version)
-                    if updated:
-                        print('Updated version {} in {}'.format(filename, path))
+                        date = date.strftime("%d/%m/%Y %H:%M:%S")
+                        updated = update_meta_tags.update_date(path, date)
+                        if updated:
+                            print('Updated date {} in {}'.format(filename, path))
+
+                        version = __format_record__constant__day__commit_count(metadata, major_version)
+                        updated = update_meta_tags.update_version(path, version)
+                        if updated:
+                            print('Updated version {} in {}'.format(filename, path))
